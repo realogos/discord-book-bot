02eb5c58cc5dfc59192f4c81911a3714e62be23b
墨丘利 <2964214859@qq.com>
2025-06-20 14:36:09 +0800

    Update bookrobot.py

--- bookrobot.py
+++ bookrobot.py
@@ -5,7 +5,6 @@ import requests
 TOKEN = 'MTM4NTQ4ODUxMDMwMzM0MjY2Mw.Gf0F9m.jL1hKb4UxGpHxH3Xs7Wrvt7lUx1_LuBBdRXccE'
 GIST_RAW_URL = ' https://gist.githubusercontent.com/realogos/15cf6185178ead6ee7fa637b1a5f9a1b/raw/5e6c28e23ee16c584e62221598cdc8a0af6cb8e9/The%2520Tower%2520of%2520Babel_texts.txt'
 
-# 获取文本块
 def fetch_text_blocks():
     response = requests.get(GIST_RAW_URL)
     if response.status_code == 200:
@@ -18,7 +17,7 @@ client = discord.Client(intents=intents)
 
 @client.event
 async def on_ready():
-    print(f'布克机器人 已上线：{client.user}')
+    print(f'{client.user}')
 
 @client.event
 async def on_message(message):
@@ -32,6 +31,6 @@ async def on_message(message):
         if blocks:
             await message.channel.send(random.choice(blocks))
         else:
-            await message.channel.send("此书架为空，请等待更新。")
+            await message.channel.send('null')
 
 client.run(TOKEN)
